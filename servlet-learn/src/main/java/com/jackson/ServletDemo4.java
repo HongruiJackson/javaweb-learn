c64d85c787c9c3b0bf9e03b56340734a4a3dcea7
JacksonZHR <1024060689@qq.com>
2024-05-21 21:35:41 +0800

    feat: 请求转发

---
+++ servlet-learn/src/main/java/com/jackson/ServletDemo4.java
@@ -0,0 +1,25 @@
+package com.jackson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo4")
+public class ServletDemo4 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("demo4...");
+
+        // 请求转发
+        req.getRequestDispatcher("/demo5").forward(req,resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
