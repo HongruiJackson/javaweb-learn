26c1433b803043edddd4e00965fcb58cb01e7d96
JacksonZHR <1024060689@qq.com>
2024-05-21 22:01:55 +0800

    feat: 响应字符数据以及中文

--- servlet-learn/src/main/java/com/jackson/ServletDemo6.java
+++ servlet-learn/src/main/java/com/jackson/ServletDemo6.java
@@ -6,17 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/demo6")
 public class ServletDemo6 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("resp...");
-        //1. 设置响应行
-        resp.setStatus(302);
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.write("this is a 请求");
 
-        //2. 设置响应头
-        resp.setHeader("Location","/servlet-learn/demo5");
 
 
 
